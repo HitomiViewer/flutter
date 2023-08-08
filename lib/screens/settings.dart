0d7c5e3ccfc1f89e9fc8ef1d6875dfa672dc4965
Anonymous <anonymous@example.com>
2023-08-09 03:00:40 +0900

    import & export

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:prompt_dialog/prompt_dialog.dart';
 import 'package:provider/provider.dart';
 import 'package:settings_ui/settings_ui.dart';
@@ -52,6 +55,89 @@ class _SettingScreenState extends State<SettingScreen> {
               ),
             ],
           ),
+          SettingsSection(
+            title: const Text('Favorite'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.import_export),
+                title: const Text('Import'),
+                onPressed: (context) async {
+                  final String? result = await prompt(context);
+                  if (result != null) {
+                    context.read<Store>().setFavorite(json.decode(result));
+                  }
+                },
+              ),
+              SettingsTile.navigation(
+                leading: const Icon(Icons.import_export),
+                title: const Text('Export'),
+                onPressed: (context) async {
+                  final String result =
+                      json.encode(context.watch<Store>().favorite);
+                  Clipboard.setData(ClipboardData(text: result));
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Copied to clipboard'),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+          // SettingsSection(
+          //   title: const Text('Viewer'),
+          //   tiles: <SettingsTile>[
+          //     SettingsTile.switchTile(
+          //       leading: const Icon(Icons.brightness_4),
+          //       title: const Text('Dark mode'),
+          //       switchValue: context.watch<Store>().darkMode,
+          //       onToggle: (bool value) =>
+          //           context.read<Store>().setDarkMode(value),
+          //     ),
+          //     SettingsTile.switchTile(
+          //       leading: const Icon(Icons.fullscreen),
+          //       title: const Text('Fullscreen'),
+          //       switchValue: context.watch<Store>().fullscreen,
+          //       onToggle: (bool value) =>
+          //           context.read<Store>().setFullscreen(value),
+          //     ),
+          //     SettingsTile.switchTile(
+          //       leading: const Icon(Icons.auto_awesome),
+          //       title: const Text('Auto hide'),
+          //       switchValue: context.watch<Store>().autoHide,
+          //       onToggle: (bool value) =>
+          //           context.read<Store>().setAutoHide(value),
+          //     ),
+          //     SettingsTile.switchTile(
+          //       leading: const Icon(Icons.auto_awesome),
+          //       title: const Text('Auto hide'),
+          //       switchValue: context.watch<Store>().autoHide,
+          //       onToggle: (bool value) =>
+          //           context.read<Store>().setAutoHide(value),
+          //     ),
+          //     SettingsTile.switchTile(
+          //       leading: const Icon(Icons.auto_awesome),
+          //       title: const Text('Auto hide'),
+          //       switchValue: context.watch<Store>().autoHide,
+          //       onToggle: (bool value) =>
+          //           context.read<Store>().setAutoHide(value),
+          //     ),
+          //     SettingsTile.switchTile(
+          //       leading: const Icon(Icons.auto_awesome),
+          //       title: const Text('Auto hide'),
+          //       switchValue: context.watch<Store>().autoHide,
+          //       onToggle: (bool value) =>
+          //           context.read<Store>().setAutoHide(value),
+          //     ),
+          //     SettingsTile.switchTile(
+          //       leading: const Icon(Icons.auto_awesome),
+          //       title: const Text('Auto hide'),
+          //       switchValue: context.watch<Store>().autoHide,
+          //       onToggle: (bool value) =>
+          //           context.read<Store>().setAutoHide(value),
+          //     ),
+          //   ],
+          // ),
         ],
       ),
     );
